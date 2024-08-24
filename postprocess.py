3916598e5529bf47135c0cc61344790536466ff9
arian yavari <arianyavari@protonmail.com>
2024-08-23 23:16:06 -0400

    made changes to the directory

---
+++ postprocess.py
@@ -0,0 +1,119 @@
+import numpy
+import tensorflow
+import pathlib
+import re
+
+class PostProcess:
+    
+    def remove_nones(self, data:list[str]=None, labels:list[str]=None):
+        invalid_terms = [None, 'nan', numpy.nan, 'NaN',]
+        while True:
+            indices = []
+            # if data is not None and labels is not None:
+            if type(data) == list and type(labels) == list:
+                # assert len(data) == len(labels), 'unmatching length for data and labels'
+                if len(data) != len(labels):
+                    print('during post process unmatching lengths found for data and labels!')
+                    min_len = min(len(data), len(labels))
+                    print(f'lengths do not match will try trimming to val {min_len}')
+                    data = data[:min_len]
+                    labels = labels[:min_len]
+                for i in range(len(data)):
+                    if data[i] in invalid_terms or type(data[i]) != str:
+                        indices.append(i)
+                    if labels[i] in invalid_terms or type(labels[i]) != str:
+                        indices.append(i)
+                indices = set(indices)
+                data = [data[i] for i in range(len(data)) if i not in indices]
+                labels = [labels[i] for i in range(len(labels)) if i not in indices]
+                return data, labels
+            
+            elif type(data) == list and type(labels) != list:
+                for i in range(len(data)):
+                    if data[i] in invalid_terms or type(data[i]) != str:
+                        if i not in indices:
+                            indices.append(i)
+                data = [data[i] for i in range(len(data)) if i not in indices]
+                return data
+            
+            elif type(labels) == list and type(data) != list:
+                for i in range(len(labels)):
+                    if labels[i] in invalid_terms or type(labels[i]) != str:
+                        if i not in indices:
+                            indices.append(i)
+                labels = [labels[i] for i in range(len(labels)) if i not in indices]
+                return labels
+    
+    def create_ngrams(self, data, ngrams_val=2):
+        '''defaults to creating bigrams. words are separated with whitespaces only.'''
+        
+        ngrams_vocab = []
+        ngrams = []
+        
+        for i in data:
+            tokens = i.split()
+            ngrams_tmp = [' '.join(tokens[j:j+ngrams_val]) for j in range(len(tokens)-ngrams_val+1)]
+            ngrams_vocab.extend(ngrams_tmp)
+            ngrams.append(ngrams_tmp)
+        return ngrams_vocab, ngrams
+            
+    def pad_ngrams(self, ngrams, vocab):
+        padding_token = '<PAD>'
+        vocab = sorted(set(vocab + [padding_token]))
+        ngrams_padded = tensorflow.keras.preprocessing.sequence.pad_sequences(
+            ngrams, dtype=object, padding='post', value=padding_token)
+        return ngrams_padded, vocab
+    
+    def multihot_encode(self, ngrams, vocab):
+        lookup = tensorflow.keras.layers.StringLookup(vocabulary=vocab, output_mode='multi_hot')
+        multi_hot_encoded_data = lookup(ngrams)
+        return multi_hot_encoded_data, lookup.get_vocabulary()
+
+    def custom_pattern(data:list, patterns_file:pathlib.Path='terms_to_remove.txt', 
+                       names_file:pathlib.Path='yob2023.txt'):
+        # data = postprocess(data, patterns, names)
+        # labels = postprocess(labels, patterns, names)
+        assert data, 'Data is empty!'
+        assert patterns_file.exists(), 'file not found!'
+        
+        with patterns_file.open('rt', encoding='utf-8') as fo:
+            patterns = [i.strip() for i in fo]
+            
+        with names_file.open('rt', encoding='utf-8') as fo:
+            names = [i.split(',', maxsplit=1)[0] for i in fo]
+        
+        tmp_data = []
+        for i in data:
+            if isinstance(i, str):
+                tmp_str = ' '.join(i.split())
+                tmp_data.append(tmp_str)
+            elif i is None:
+                tmp_data.append(None)
+            elif isinstance(i, tuple):
+                tmp_str = ' '.join(i).strip()
+                tmp_data.append(tmp_str)
+        
+        pattern = re.compile('|'.join(patterns), flags=re.IGNORECASE)
+        name_pattern = re.compile(r'\b(?:{})\b'.format('|'.join(map(re.escape, names))))
+        
+        flag = True
+        while flag:
+            tmp_data2 = []
+            for i in tmp_data:
+                if i is None:
+                    tmp_data2.append(None)
+                else:
+                    tmp_data2.append(pattern.sub('', i))
+                    
+            flag = tmp_data2 != tmp_data
+            tmp_data = tmp_data2
+        
+        
+        tmp_data3 = []
+        for i in tmp_data:
+            if i is None:
+                tmp_data3.append(None)
+            else:
+                tmp_data3.append(name_pattern.sub('', i))
+        
+        return tmp_data3
---
+++ preprocess.py
@@ -0,0 +1,78 @@
+import re
+import pathlib
+import shutil
+import numpy
+
+class Preprocess:
+    def __init__(self):
+        self.files_pattern = r'duties.*list'
+        
+    def move_files(self, pattern:str, dir_:pathlib.Path, suffix, ndir:str):
+        paths = list(dir_.glob(f'**/*{suffix}'))
+        if paths:
+            poi = []
+            for i in paths:
+                matches = re.findall(pattern, str(i), re.IGNORECASE)
+                if matches:
+                    poi.append(i)
+        new_path = pathlib.Path('/'.join(dir_.parts[:-1]).replace('\\', '')).joinpath(f'{dir_.parts[-1]}_{ndir}')
+        for i in poi:
+            if not new_path.exists():
+                new_path.mkdir()
+            shutil.move(i, new_path)
+        
+    def remove_special_chars(self, data):
+        pattern = r'[`~!@#$%^&*()\-_=+,.\/?;:\\|\[\]{}]'
+        cleaned_data = [re.sub(pattern, ' ', i) for i in data if i not in (None, numpy.nan,)]
+        return cleaned_data
+    
+    def custom_pattern(self, pattern, replacement, text):
+        tmp_txt = re.sub(pattern, replacement, text)
+        return tmp_txt
+    
+    def str_check(self, data):
+        for i in data:
+            if type(i) != str:
+                print(f'{i} has non-str data')
+                
+    def str_only(self, data):
+        tmp_data = []
+        for i in data:
+            if type(i) == str:
+                tmp_data.append(i)
+        return tmp_data
+                
+    def find_non_utf8(self, data):
+        for i, text in enumerate(data):
+            try:
+                if type(text) == str:
+                    text = str.encode(text)
+                else: 
+                    continue
+            except UnicodeEncodeError as e:
+                print(f"Non-UTF-8 character found in text at index {i}: {text[e.start:e.end]}")
+                
+    def clean_non_utf8(self, data):
+        cleaned_data = [i.encode('utf-8', 'ignore').decode('utf-8', 'ignore') for i in data]
+        cleaned_data = [re.sub(r'[^\x00-\x7F]+', '', i) for i in cleaned_data]
+        return cleaned_data
+    
+    def retrieve_basic_info(self, data, info_needed):
+        if info_needed == 'email':
+            pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+        elif info_needed == 'phone':
+            pattern = r'\+?\d{1,2}?[ .-]?\(?\d{3}\)?[ .-]?\d{3}[ .-]?\d{4}'
+        elif info_needed == 'name':
+            pattern = r'\b[A-Z][a-z]*\s[A-Z][a-z]*(?:\s[A-Z][a-z]*)?\b'
+        else:
+            print('requested pattern is not available!')
+            return []
+        return re.findall(pattern, data)
+    
+    def extract_info(self, data):
+        return {
+            'names' : self.retrieve_basic_info(data, 'name'),
+            'emails' : self.retrieve_basic_info(data, 'email'),
+            'phones' : self.retrieve_basic_info(data, 'phone')
+        }
+        
